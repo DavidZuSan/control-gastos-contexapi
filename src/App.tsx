c24e74cd1cb1b77f71206359907a88aa0ab8df15
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2024-05-22 09:53:37 +0200

    feat: Add @headlessui/react and @heroicons/react dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useMemo } from "react";
 import BudgetForm from "./components/BudgetForm";
+import { useBudget } from "./hooks/useBudget";
+import BudgetTacker from "./components/BudgetTacker";
+import ExpenseModal from "./components/ExpanseModal";
 
 function App() {
+  const { state } = useBudget();
+
+  const isValidBudget = useMemo(() => state.budget > 0, [state.budget]);
+
   return (
     <>
       <header className="bg-blue-600 py-8 max-h-72">
@@ -10,8 +18,13 @@ function App() {
       </header>
 
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        <BudgetForm />
+        {isValidBudget ? <BudgetTacker /> : <BudgetForm />}
       </div>
+      {isValidBudget && (
+        <main className="max-w-3xl mx-auto py-10">
+          <ExpenseModal />
+        </main>
+      )}
     </>
   );
 }
---
+++ src/components/ExpenseForm.tsx
@@ -0,0 +1,61 @@
+import { categories } from "../data/categories";
+
+export default function ExpenseForm() {
+  return (
+    <form className="space-y-5">
+      <legend className=" uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
+        Nuevo Gasto
+      </legend>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="expenseName" className="text-xl">
+          Nombre Gasto:
+        </label>
+        <input
+          type="text"
+          id="expenseName"
+          placeholder="Agrega el Nombre del Gastos"
+          className="bg-slate-100 p-2"
+          name="expenseName"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="amount" className="text-xl">
+          Cantidad:
+        </label>
+        <input
+          type="number"
+          id="amount"
+          placeholder="Agrega la Cantidad del Gasto: $300.40"
+          className="bg-slate-100 p-2"
+          name="expenseName"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="category" className="text-xl">
+          Categoria:
+        </label>
+        <select
+          id="category"
+          placeholder="Añade la cantaidad del gasto: ej. 300"
+          className="bg-slate-100 p-2"
+          name="category"
+        >
+          <option value="">-- Seleccione --</option>
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <input
+        type="submit"
+        className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
+        value={"Registrar Gasto"}
+      />
+    </form>
+  );
+}
